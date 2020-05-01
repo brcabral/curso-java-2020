7340f6f66897a6806a333cd60a6926327be7d472
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-05-01 10:49:31 -0300

    Primeiro commit - projeto criado

---
+++ exercicios/source/Session26/src/test/java/com/educandoweb/workshopmongodb/Session26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.workshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Session26ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
